aa0fbf460c4867a023850f8ca5b7b73d3795179f
Anas Katoot <anas1243@gmail.com>
2023-03-04 11:50:05 +0200

    doc: Add readme file

--- output.tf
+++ output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
-  value = "aws_vpc.${var.use_case}_vpc.id"
+  value = aws_vpc.vpc.id
 }
 
 output "public_subnet1_id" {
-  value = "aws_subnet.${var.use_case}_public_subnet1.id"
+  value = aws_subnet.public_subnet1.id
 }
 
 output "public_subnet2_id" {
-  value = "aws_subnet.${var.use_case}_public_subnet2.id"
+  value = aws_subnet.public_subnet2.id
 }
 
 output "private_subnet1_id" {
-  value = "aws_subnet.${var.use_case}_private_subnet1.id"
+  value = aws_subnet.private_subnet1.id
 }
 
 output "private_subnet2_id" {
-  value = "aws_subnet.${var.use_case}_private_subnet2.id"
+  value = aws_subnet.private_subnet2.id
 }
